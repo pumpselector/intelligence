dc866a98a3d57766726baee7636bfec24aad9902
tekliflix <tekliflix@gmail.com>
2026-08-19 20:32:49 +0200

    Intelligence ve news sayfalari dinamik render'a cevrildi

--- app/intelligence/page.tsx
+++ app/intelligence/page.tsx
@@ -1,6 +1,8 @@
 import { getAllDealers } from "@/lib/dealers";
 import IntelligenceClient from "@/components/IntelligenceClient";
 
+export const dynamic = "force-dynamic";
+
 export default async function IntelligencePage() {
   const dealers = await getAllDealers();
 
--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,6 +1,8 @@
 import { getPublishedNews } from "@/lib/news";
 import NewsClient from "@/components/NewsClient";
 
+export const dynamic = "force-dynamic";
+
 export default async function NewsPage() {
   const news = await getPublishedNews();
 
